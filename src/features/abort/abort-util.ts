5642259d712e9dcd968f31baa62bb492e3f6cb0d
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-09-09 11:48:11 +0200

    Remove as any cast

--- src/features/abort/abort-util.ts
+++ src/features/abort/abort-util.ts
@@ -20,7 +20,7 @@ export function mergeSignals(...signals: AbortSignal[]): AbortSignal {
     }
     signal.addEventListener('abort', function (_ev: Event) {
       checkSignalAndAbort(this, abortCtrl);
-    }, { signal: abortCtrl.signal, once: true });
+    }, { once: true, signal: abortCtrl.signal });
   }
 
   return abortCtrl.signal;
@@ -33,14 +33,14 @@ function checkSignalAndAbort(signal: AbortSignal, abortCtrl: AbortController): v
 
 export function fixFirefoxAbortError(err: any): any {
   return (err.name !== 'AbortError') ? createError('AbortError', 'The user aborted a request.', {
-      original: err as any
+      original: err
     })
     : err;
 }
 
 export function fixChromiumAndWebkitTimeoutError(err: any): any {
   return (err.name !== 'TimeoutError') ? createError('TimeoutError', 'The operation timed out.', {
-      original: err as any
+      original: err
     })
     : err;
 }
