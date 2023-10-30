5a79a95be3b6f0d00b3e6099392cfc9fef30dc16
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-10-30 20:15:12 +0100

    Add abort unit tests

--- tests/units/features/abort/abort-util.spec.ts
+++ tests/units/features/abort/abort-util.spec.ts
@@ -1,5 +1,6 @@
-import { defaultSignal, timedSignal } from '../../../../src/features/abort/abort-util';
-import { expectProperty, expectType } from '../../../fixtures/utils/expect-util';
+import { defaultSignal, mergeSignals, timedSignal } from '../../../../src/features/abort/abort-util';
+import { sleep } from '../../../../src/utils/promise-util';
+import { expectEqual, expectProperty, expectType } from '../../../fixtures/utils/expect-util';
 
 describe('Util - Abort', () => {
 
@@ -18,5 +19,42 @@ describe('Util - Abort', () => {
       expectType(signal, 'AbortSignal');
       expectProperty(signal, 'hasTimeout', 'boolean', true);
     });
+  });
+
+  describe('mergeSignals', () => {
+
+    it('should return an aborted signal', () => {
+      const abortCtrl1: AbortController = new AbortController();
+      const abortCtrl2: AbortController = new AbortController();
+
+      const reason: string = 'reason';
+
+      abortCtrl1.abort(reason);
+
+      const { signal } = mergeSignals(abortCtrl1.signal, abortCtrl2.signal);
+
+      expectEqual(signal.aborted, true);
+      expectEqual(signal.reason, reason);
+    });
+
+    it('should abort because of timeout', async () => {
+      const signal1: AbortSignal = new AbortController().signal;
+      const signal2: AbortSignal = timedSignal(0);
+
+      await sleep(0);
+
+      const { signal } = mergeSignals(signal1, signal2);
+
+      expectEqual(signal.aborted, true);
+      expectEqual(signal.abortedByTimeout, true);
+    });
+  });
+
+  describe('fixFirefoxAbortError', () => {
+
+  });
+
+  describe('fixFirefoxAbortError', () => {
+
   });
 });
