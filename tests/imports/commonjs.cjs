a44e99bc824309792b03a83897b33ae0cdea7726
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2023-12-30 18:18:05 +0100

    Improve import tests

--- tests/imports/commonjs.cjs
+++ tests/imports/commonjs.cjs
@@ -1,3 +1,12 @@
 const drino = require('drino');
+const { HttpErrorResponse } = require('drino');
 
 const instance = drino.create({});
+
+const error = new HttpErrorResponse({
+  error: '',
+  url: 'http://localhost:8080',
+  headers: new Headers(),
+  status: 0,
+  statusText: '',
+});
--- tests/imports/esm.mjs
+++ tests/imports/esm.mjs
@@ -1,3 +1,11 @@
-import drino from 'drino';
+import drino, { HttpErrorResponse } from 'drino';
 
 const instance = drino.create({});
+
+const error = new HttpErrorResponse({
+  error: '',
+  url: 'http://localhost:8080',
+  headers: new Headers(),
+  status: 0,
+  statusText: '',
+});
