f7226ce17b54b5a37d88a7ec0434e6a94a759fff
Rémy Abitbol <95510893+remscodes@users.noreply.github.com>
2024-06-08 01:59:16 +0200

    Export missing types

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export type { DrinoInstance } from './drino.model';
 export type { DrinoPlugin, PluginTools } from './plugin.model';
+export type { QueryParamsType, HeadersType } from './http.model';
